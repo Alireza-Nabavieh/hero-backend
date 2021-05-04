f07429e21291d953b45c06d31a4b0bccea8f851a
a.nabavieh <nabavieh@gmail.com>
2021-05-04 13:17:46 +0430

    change postgres

---
+++ api/events/config/env/production/server.js
@@ -0,0 +1,17 @@
+// module.exports=({env})=>({
+//     url: env('STRAPI_URL')
+// })
+
+
+
+// -----------> for strapi
+
+// http url: /api
+//  build ENV_NODE=production yarn build
+// STRAPi_URL= ${APP_URL}/api
+//  DATABASE_URL= ${db.DATABASE_URL}
+ 
+
+// -------------> for next
+// http url: /
+// API_URL=${APP_URL}/api
