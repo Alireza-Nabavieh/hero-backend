54d09e07f1004c53f58b42db322d2e350f16a8f1
a.nabavieh <nabavieh@gmail.com>
2021-05-04 14:34:53 +0430

    change dev

--- config/database.js
+++ config/database.js
@@ -4,20 +4,38 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'purchase'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Ali87243210115'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
-        // ssl: {
-        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        // },
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: env.bool('DATABASE_SSL', false),
+        useNullAsDefault: true,
       },
     },
   },
 });
+
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'purchase'),
+//         username: env('DATABASE_USERNAME', 'postgres'),
+//         password: env('DATABASE_PASSWORD', 'Ali87243210115'),
+//         schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+//         // ssl: {
+//         //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//         // },
+//       },
+//       options: {
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//     },
+//   },
+// });
